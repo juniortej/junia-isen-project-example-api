fa8bfd05075e4b41d526820fd8788c60d0831102
guayben <clement.calujek@gmail.com>
2024-11-25 12:21:02 +0100

    oui

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Welcome to the Shop API!"})
+    return jsonify({"message": "Je souhaite me donner la mort dans les plus bref delais"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
