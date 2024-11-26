2eb2cb729bf0de782c512c7d46772cb45ba4ae99
Alexis Mallet <alexis.mallet@student.junia.com>
2024-11-26 14:17:54 +0100

    feat: edit api and test signed commit

--- api/app.py
+++ api/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Welcome to the Shop API!"})
 
+@app.route("/items")
+def items():
+    return jsonify({"message": "List of items"})
+
+@app.route("/baskets")
+def baskets():
+    return jsonify({"message": "List of baskets"})
+
+@app.route("/users")
+def users():
+    return jsonify({"message": "List of users"})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- api/tests/test_app.py
+++ api/tests/test_app.py
@@ -11,3 +11,18 @@ def test_home(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json == {"message": "Welcome to the Shop API!"}
+    
+def test_items(client):
+    response = client.get("/items")
+    assert response.status_code == 200
+    assert response.json == {"message": "List of items"}
+    
+def test_baskets(client):
+    response = client.get("/baskets")
+    assert response.status_code == 200
+    assert response.json == {"message": "List of baskets"}
+    
+def test_users(client):
+    response = client.get("/users")
+    assert response.status_code == 200
+    assert response.json == {"message": "List of users"}
